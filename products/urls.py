8e5c9f57e5ef65e6207a1316041837df10de585c
Ismar <ismar110501@gmail.com>
2023-09-26 14:15:29 +0600

    detail view for product

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.product, name='products'),
     path('create/', views.createproduct, name='create'),
+    path("<int:pk>", views.ProductDetail.as_view(), name='product_detail')
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView
 from .models import Product
 from .forms import ProductForm
 
 
+
 # Create your views here.
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'products/detail_product.html'
+    context_object_name = 'thing'
 def product(request):
     products = Product.objects.all()
     return render(request, 'products/products.html', {"products": products})
